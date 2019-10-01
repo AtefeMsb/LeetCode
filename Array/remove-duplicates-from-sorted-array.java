7c7cacade119953386b9bbc922b75ac2e04f3bf1
Atefe Mosayebi <atefe.mosayebi@gmail.com>
2019-10-01 12:54:52 -0700

    Update remove-duplicates-from-sorted-array.java

--- Array/remove-duplicates-from-sorted-array.java
+++ Array/remove-duplicates-from-sorted-array.java
@@ -1,9 +1,18 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
         
+        // special cases
+        if (nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return 1;
+        }
+        
         int length = 1;
         int currentIndex = 0;
         
+        // check each new element with the currentIndex for similarity
         for (int i = 1; i < nums.length; i++) {
             if (nums[i] == nums[currentIndex]) {
                 continue;
@@ -13,7 +22,7 @@ class Solution {
             currentIndex++;
             length++;
         }
-    
+        
         return length;
         
     }
