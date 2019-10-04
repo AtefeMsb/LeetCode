be0679e532a8ecb522e67e72c49a5e65d97650ca
Atefe Mosayebi <atefe.mosayebi@gmail.com>
2019-10-03 17:02:36 -0700

    Create best-time-to-buy-and-sell-stock.java

---
+++ Array/best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,24 @@
+/*
+update min (price of buy) in each iteration if current value is less than previous min value
+ELSE calculate profit for this day and copare it with current profit and update the profit
+*/
+class Solution {
+    public int maxProfit(int[] prices) {
+        
+        // profit made so far
+        int profit = 0;
+        int min = Integer.MAX_VALUE;
+        
+        for (int i = 0; i < prices.length; i++) {
+            // update the buy price
+            if (prices[i] < min) {
+                min = prices[i];
+                // calculate the profit at this day
+            } else { 
+                profit = Math.max (profit , prices[i] - min); 
+            }
+        }
+        
+        return profit;
+    }
+}
