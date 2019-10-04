f40e4bf7a7db47fc3e6cb0f1f8de7f1d7a94fb4c
Atefe Mosayebi <atefe.mosayebi@gmail.com>
2019-10-03 18:12:40 -0700

    Create two-sum-||-input-array-is-sorted.java

---
+++ Array/two-sum-||-input-array-is-sorted.java
@@ -0,0 +1,21 @@
+// Your returned answers (both index1 and index2) are not zero-based. --> i + 1
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        
+        int[] result = new int[2];
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        
+        for (int i = 0; i < numbers.length; i++) {
+            
+            if (freqMap.containsKey(target - numbers[i])) {
+                result[0] = freqMap.get(target - numbers[i]);
+                result[1] = i + 1;
+                break;
+            }
+            
+            freqMap.put(numbers[i], i + 1);
+        }
+        
+        return result;
+    }
+}
