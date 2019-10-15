053e3c65555de39c72f00805947af53494fb21e0
Atefe Mosayebi <atefe.mosayebi@gmail.com>
2019-10-15 11:20:47 -0700

    Create fibonacci-number.java

---
+++ Array/fibonacci-number.java
@@ -0,0 +1,32 @@
+// class Solution {
+//     public int fib(int N) {
+        
+//         if (N == 0) return 0;
+//         if (N == 1) return 1;
+//         return fib (N - 1) + fib (N - 2);
+//     }
+// }
+
+// Iterative Solution
+class Solution {
+    public int fib(int N) {
+        
+        if (N == 0) return 0;
+        if (N == 1) return 1;
+        
+        int a = 0;
+        int b = 1;
+        int sum = 0;
+        
+        while (N > 1) {
+            
+            sum = a + b;
+            a = b;
+            b = sum;
+            
+            N--;
+        }
+        
+        return sum;
+    }
+}
