766723400197cb0f4eb4af532e3e97436d2df6bf
Atefe Mosayebi <atefe.mosayebi@gmail.com>
2019-11-13 12:38:32 -0800

    Update remove-nth-node-from-end-of-list.java

--- LinkedList/remove-nth-node-from-end-of-list.java
+++ LinkedList/remove-nth-node-from-end-of-list.java
@@ -7,17 +7,20 @@ class Solution {
         ListNode slow = dummy;
         ListNode fast = dummy;
         
-    
+        // slow node must stop at previous node of the node we want to delete
+        // so the distance between slow and fast node must be n + 1
         for (int i = 1; i <= n + 1; i++) {
             fast = fast.next;
         }
         
         // diffrence between slow and fast is n + 1
+        // when fast reach the null means slow is n + 1 steps behind
         while (fast != null) {
             fast = fast.next;
             slow = slow.next;
         }
       
+        // deletion
         slow.next = slow.next.next;
         
         return dummy.next;
