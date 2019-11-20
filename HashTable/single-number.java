a16d9522dc49e3c519ddc5f626a8f82e4166b26e
Atefe Mosayebi <atefe.mosayebi@gmail.com>
2019-11-20 13:40:30 -0800

    Create single-number.java

---
+++ HashTable/single-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        
+        // print the map
+        // map.forEach((key, value) -> System.out.println(key + ":" + value));
+        
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1) {
+                return entry.getKey();
+            }
+        }
+
+        return -1;
+    }
+}
