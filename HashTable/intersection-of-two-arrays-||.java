828a9e50b1c457d36e636914c50bddbcad6d9430
Atefe Mosayebi <atefe.mosayebi@gmail.com>
2019-11-21 13:36:32 -0800

    Update intersection-of-two-arrays-||.java

--- HashTable/intersection-of-two-arrays-||.java
+++ HashTable/intersection-of-two-arrays-||.java
@@ -25,3 +25,36 @@ class Solution {
         return arrResult;
     }
 }
+
+// sort
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        
+        int i = 0;
+        int j = 0; 
+        List<Integer> listResult = new ArrayList<>();
+        
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] < nums2[j]) {
+                i++;
+            } else if (nums1[i] > nums2[j]) {
+                j++;
+            } else {
+                listResult.add(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+        
+        // convert list<Integer> to int[]
+        int[] arrResult = new int[listResult.size()];
+        for (int k = 0; k < listResult.size(); k++) {
+            arrResult[k] = listResult.get(k);
+        }
+        
+        return arrResult;  
+    }
+}
