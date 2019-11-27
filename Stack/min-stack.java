ae71b72ec9c8d9c7df71ae0397b30cedd900f899
Atefe Mosayebi <atefe.mosayebi@gmail.com>
2019-11-26 16:38:19 -0800

    Create min-stack.java

---
+++ Stack/min-stack.java
@@ -0,0 +1,102 @@
+// TWO STACKS
+/*
+class MinStack {
+    // stack: store the stack numbers
+    private Stack<Integer> stack = new Stack<Integer>();
+    // minStack: store the current min values
+    private Stack<Integer> minStack = new Stack<Integer>();
+    
+    public void push(int x) {
+        // store current min value into minStack
+        if (minStack.isEmpty() || x <= minStack.peek())
+            minStack.push(x);
+        stack.push(x);
+    }
+
+    public void pop() {
+        // use equals to compare the value of two object, if equal, pop both of them
+        if (stack.peek().equals(minStack.peek()))
+            minStack.pop();
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
+*/
+
+// ONE STACK - pushing the second min always before the new min
+/*
+class MinStack {
+    int min = Integer.MAX_VALUE;
+    Stack<Integer> stack = new Stack<Integer>();
+    public void push(int x) {
+        // only push the old minimum value when the current 
+        // minimum value changes after pushing the new value x
+        if(x <= min){          
+            stack.push(min);
+            min=x;
+        }
+        stack.push(x);
+    }
+
+    public void pop() {
+        // if pop operation could result in the changing of the current minimum value, 
+        // pop twice and change the current minimum value to the last minimum value.
+        if(stack.pop() == min) min=stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return min;
+    }
+}
+*/
+
+// LINKEDLIST
+class MinStack {
+    private Node head;
+    
+    public void push(int x) {
+        if(head == null) 
+            head = new Node(x, x);
+        else 
+            head = new Node(x, Math.min(x, head.min), head);
+    }
+
+    public void pop() {
+        head = head.next;
+    }
+
+    public int top() {
+        return head.val;
+    }
+
+    public int getMin() {
+        return head.min;
+    }
+    
+    private class Node {
+        int val;
+        int min;
+        Node next;
+        
+        private Node(int val, int min) {
+            this(val, min, null);
+        }
+        
+        private Node(int val, int min, Node next) {
+            this.val = val;
+            this.min = min;
+            this.next = next;
+        }
+    }
+}
