3da5f81060e9798897dd2f6b888430230194218d
Atefe Mosayebi <atefe.mosayebi@gmail.com>
2019-12-02 17:37:01 -0800

    Create implement-queue-using-stacks.java

---
+++ Queue/implement-queue-using-stacks.java
@@ -0,0 +1,46 @@
+class MyQueue {
+
+    Stack<Integer> pushStack;
+    Stack<Integer> popStack;
+
+    /** Initialize your data structure here. */
+    public MyQueue() {
+        pushStack = new Stack();
+        popStack = new Stack();
+    }
+    
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        pushStack.push(x);
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        peek();
+        return popStack.pop();
+    }
+    
+    /** Get the front element. */
+    public int peek() {
+        if (popStack.isEmpty()) {
+            while (!pushStack.isEmpty()) {
+                popStack.push(pushStack.pop());
+            }
+        }
+        return popStack.peek();
+    }
+    
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return pushStack.isEmpty() && popStack.isEmpty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
