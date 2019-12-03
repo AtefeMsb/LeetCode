7b1a4d0b1b2488f4db2edf97789479ba3d16b93e
Atefe Mosayebi <atefe.mosayebi@gmail.com>
2019-12-02 18:27:52 -0800

    Create implement-stack-using-queue.java

---
+++ Stack/implement-stack-using-queue.java
@@ -0,0 +1,75 @@
+// Two Queues -- push O(1), pop O(n)
+/*
+class MyStack {
+
+    private Queue<Integer> q1;
+    private Queue<Integer> q2;
+    private int top;
+    
+    public MyStack() {
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
+    }
+    
+    public void push(int x) {
+        q1.offer(x);
+        top = x;
+    }
+    
+    public int pop() {
+        int theTop = top;
+        while (q1.size() > 1) {
+            top = q1.poll();
+            q2.add(top);
+        }
+        
+        q1.poll();
+        
+        Queue<Integer> temp = q1;
+        q1 = q2;
+        q2 = temp;
+        
+        return theTop;
+    }
+    
+    public int top() {
+        return top;
+    }
+    
+    public boolean empty() {
+        return q1.isEmpty();
+    }
+}
+*/
+    
+// One Queues -- push O(n), pop O(1)
+class MyStack {
+
+    private Queue<Integer> q;
+ 
+    
+    public MyStack() {
+        q = new LinkedList<>();
+    }
+    
+    public void push(int x) {
+        q.offer(x);
+        int n = q.size();
+        while (n > 1) {
+            q.offer(q.poll());
+            n--;
+        }
+    }
+    
+    public int pop() {
+        return q.poll();
+    }
+    
+    public int top() {
+        return q.peek();
+    }
+    
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
