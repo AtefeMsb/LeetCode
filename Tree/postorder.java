89702ff5f58d3b40bfd4b2822dd9bd9430d1e0b2
Atefe Mosayebi <atefe.mosayebi@gmail.com>
2019-12-05 13:01:25 -0800

    Update postorder.java

--- Tree/postorder.java
+++ Tree/postorder.java
@@ -50,6 +50,8 @@
     }
 
     // manipulating the result
+    // Iterative push to the begining of array
+    // this will result shifting after each push.
     public List<Integer> postorderTraversal(TreeNode root) {
         
         Stack<TreeNode> stack = new Stack();
@@ -75,6 +77,42 @@
           if (node.right != null) {
             stack.add(node.right);
           }
+        }
+        return results;
+    }
+
+// iterative using set and stack
+public List<Integer> postorderTraversal(TreeNode root) {
+        
+        // one set - one stack
+        Stack<TreeNode> stack = new Stack();
+        Set<TreeNode> set = new HashSet<>();
+        List<Integer> results = new ArrayList<>();
+        
+        if (root == null) {
+            return results;
+        }
+        
+        stack.push(root); 
+        while (!stack.isEmpty()) {
+            TreeNode cur = stack.pop();
+            
+            if (!set.contains(cur)) {
+                stack.push(cur);
+                
+                if (cur.right != null) {
+                    stack.push(cur.right);
+                }
+                
+                if (cur.left != null) {
+                    stack.push(cur.left);
+                } 
+                
+                set.add(cur);
+                
+            } else {
+                results.add(cur.val);
+            }
         }
         return results;
     }
