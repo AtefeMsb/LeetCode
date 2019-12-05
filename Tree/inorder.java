90bd008585c45a562940c7d688ee050b95c052b8
Atefe Mosayebi <atefe.mosayebi@gmail.com>
2019-12-04 16:01:23 -0800

    Create inorder.java

---
+++ Tree/inorder.java
@@ -0,0 +1,35 @@
+    // iterative
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack();
+        
+        TreeNode cur = root;
+        
+        while (cur != null || !stack.isEmpty()) {
+            // see all the lefts
+            while (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            }
+            
+            cur = stack.pop();
+            result.add(cur.val);
+            cur = cur.right;
+        }
+        return result;
+    } 
+    
+     // recursive
+    private void inorderTraversal(TreeNode root, List result) {
+        if (root == null) {
+            return;
+        }
+        inorderTraversal(root.left, result);
+        result.add(root.val);
+        inorderTraversal(root.right, result);
+    }
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        inorderTraversal(root, result);
+        return result;
+    }
