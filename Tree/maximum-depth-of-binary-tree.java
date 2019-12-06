0edc4eae8d04592e14a9e1726faf547c6e51d28a
Atefe Mosayebi <atefe.mosayebi@gmail.com>
2019-12-05 20:52:29 -0800

    Create maximum-depth-of-binary-tree.java

---
+++ Tree/maximum-depth-of-binary-tree.java
@@ -0,0 +1,65 @@
+ // bfs
+ public int maxDepth(TreeNode root) {
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        
+        if (root == null) {
+            return 0;
+        }
+        
+        int level = 0;
+        
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                
+                TreeNode cur = queue.poll();
+                if (cur.left != null) { queue.offer(cur.left); }
+                if (cur.right != null) { queue.offer(cur.right); }
+                
+            }
+            
+            level++;
+        }
+        
+        return level;    
+    }
+    
+    // recursive - "Bottom-up" Solution
+    public int maxDepth(TreeNode root) {
+        
+        if (root == null) {
+            return 0;
+        }
+        
+        int leftMax = maxDepth(root.left);
+        int rightMax = maxDepth(root.right);
+        return Math.max(leftMax , rightMax) + 1;
+          
+    }
+    
+    // recursive - "Top-down" Solution
+    
+     private int answer = 0;
+    
+    // recursive - top to bottom
+    public int maxDepth(TreeNode root) {
+        // depth of root is 1
+        maximum_depth(root, 1);
+        return answer;
+    } 
+    
+    	
+    
+    private void maximum_depth(TreeNode root, int depth) {
+        if (root == null) {
+            return;
+        }
+        if (root.left == null && root.right == null) {
+            answer = Math.max(answer, depth);
+        }
+        maximum_depth(root.left, depth + 1);
+        maximum_depth(root.right, depth + 1);
+    }
+    
