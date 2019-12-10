8a02890ffa2135c19d5c7dff89a1c7729e92994f
Atefe Mosayebi <atefe.mosayebi@gmail.com>
2019-12-10 12:42:58 -0800

    Create lowest-common-ancestor-of-binary-tree.java

---
+++ Tree/lowest-common-ancestor-of-binary-tree.java
@@ -0,0 +1,38 @@
+   // recursive
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        
+        /*
+        base cases:
+        When we have an answer.
+            1.) If the node we are holding is null then we can't search...just return null
+            2.) If we find either value return ourselves to the caller
+        */
+        if (root == null) return null;
+        if (root.val == p.val || root.val == q.val) return root;
+
+        /*
+        'root' doesn't satisfy any of our base cases.
+        Search left and then search right.
+        */
+        TreeNode leftSearchResult = lowestCommonAncestor(root.left, p, q);
+        TreeNode rightSearchResult = lowestCommonAncestor(root.right, p, q);
+
+        /*
+        If nothing turned up on the left, return whatever we got
+        back on the right.
+        */
+        if (leftSearchResult == null) return rightSearchResult;
+
+        /*
+        If nothing turned up on the right, return whatever we got
+        back on the left.
+        */
+        if (rightSearchResult == null) return leftSearchResult;
+
+        /*
+        If we reach here that means we got something back on the left AND
+        right...that means this node is the LCA...because our recursion returns
+        from bottom to up...so we return what we hold...'root'
+        */
+        return root;
+    }
