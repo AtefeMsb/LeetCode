ffa5495a3d7c9a44cf2feb29b2aebb8d0d542766
Atefe Mosayebi <atefe.mosayebi@gmail.com>
2019-12-10 16:08:19 -0800

    Update serialize-and-deserialize-binary-tree.java

--- Tree/serialize-and-deserialize-binary-tree.java
+++ Tree/serialize-and-deserialize-binary-tree.java
@@ -1,63 +1,54 @@
 public class Codec {
-
-    // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-        
-        StringBuilder tree = new StringBuilder();
+        if (root == null) return "";
         Queue<TreeNode> queue = new LinkedList<>();
+        StringBuilder tree = new StringBuilder();
         
-        if (root == null)  return "";
-        queue.offer(root);
-        
+        queue.add(root);
         while (!queue.isEmpty()) {
             TreeNode cur = queue.poll();
             
             if (cur == null) {
                 tree.append("null,");
                 continue;
-            } 
-
+            }
             tree.append(cur.val);
-            tree.append(",");               
-
-            if (cur.left != null) { queue.offer(cur.left); }
-            if (cur.right != null) { queue.offer(cur.right); }
+            tree.append(",");
+            
+            // even null nodes gets push in the queue
+            queue.add(cur.left);
+            queue.add(cur.right);
         }
-        
-        return tree.toString();    
+        return tree.toString();
     }
 
-    // using queue and bfs technique
-      public TreeNode deserialize(String data) {
-          if (data == "") return null;
-          Queue<TreeNode> queue = new LinkedList<>();
-          String[] values = data.split(",");
-          
-          TreeNode root = new TreeNode(Integer.parseInt(values[0]));
-          
-          // add original root to the queue
-          queue.offer(root);
-          
-          // only not-null nodes gets added to queue
-          // for each node removed from queue check two items from values
-          for (int  i = 1; i < values.length; i++) {
-              TreeNode cur = queue.poll();
-              
-              // check i
-              if (!values[i].equals("null")) {
-                  TreeNode left = new TreeNode(Integer.parseInt(values[i]));
-                  cur.left = left;
-                  queue.offer(left);
-              }
-              
-              // check i + 1
-               if (!values[++i].equals("null")) {
-                  TreeNode right = new TreeNode(Integer.parseInt(values[i]));
-                  cur.right = right;
-                  queue.offer(right);
-              }
-          }
-    
-          return root;
+    public TreeNode deserialize(String data) {
+        
+        if (data == "") return null;
+        Queue<TreeNode> queue = new LinkedList<>();
+        String[] values = data.split(",");
+        
+        TreeNode root = new TreeNode(Integer.parseInt(values[0]));
+        queue.add(root);
+        
+        for (int i = 1; i < values.length; i++) {
+            
+            TreeNode parent = queue.poll();
+            
+            // i
+            if (!values[i].equals("null")) {
+                TreeNode left = new TreeNode(Integer.parseInt(values[i]));
+                parent.left = left;
+                queue.add(left);
+            }
+            
+            // i + 1
+            if (!values[++i].equals("null")) {
+                TreeNode right = new TreeNode(Integer.parseInt(values[i]));
+                parent.right = right;
+                queue.add(right);
+            }
+        }
+        return root;
     }
 }
