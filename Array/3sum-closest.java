75ecfd1d0a867e8b217b19e5f132381443ed21a2
Atefe <atefe.mosayebi@gmail.com>
2021-01-06 14:55:28 -0800

    max conecutive ones

--- Array/3sum-closest.java
+++ Array/3sum-closest.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
         
--- Array/max-consecutive-ones.java
+++ Array/max-consecutive-ones.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     public int findMaxConsecutiveOnes(int[] nums) {
         
@@ -22,4 +23,25 @@ class Solution {
         return Math.max(max, currentMax);
     }
     
+}
+
+*/
+
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        
+        int maxLength = 0;
+        int currentLength = 0;
+        
+        for (int num : nums) {
+            if (num == 1) {
+                currentLength += 1;
+                maxLength = Math.max(maxLength, currentLength);
+            } else {
+                currentLength = 0;  
+            }
+        }
+        
+        return maxLength;
+    }
 }
