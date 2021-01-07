09528d22d416b1295475f207ae6d8af6fda4376b
Atefe <atefe.mosayebi@gmail.com>
2021-01-07 15:31:33 -0800

    solved bunch of Array questions

--- Array/remove-element.java
+++ Array/remove-element.java
@@ -1,18 +1,35 @@
+
+// Approach 1: Two Pointers
 class Solution {
     public int removeElement(int[] nums, int val) {
-    
-        int left = 0;
-        int right = nums.length - 1;
         
-        while (left <= right) {
-            if (nums[left] == val) {
-                nums[left] = nums[right];
-                right--;
-            } else {
-                left++;
+        int slow = 0;
+        for (int fast = 0; fast < nums.length; fast++) {
+            if (nums[fast] != val) {
+                nums[slow] = nums[fast];
+                slow++;
             }
-             
         }
-        return left;
+        
+        return slow;
+    }
+}
+
+
+// Approach 2: Two Pointers - when elements to remove are rare
+class Solution2 {
+    public int removeElement(int[] nums, int val) {
+        int i = 0;
+        int n = nums.length;
+        while (i < n) {
+            if (nums[i] == val) {
+                nums[i] = nums[n - 1];
+                n--;
+            } else{
+                i++;
+            }
+        }
+        
+        return n;
     }
 }
