9d97960f974b0157f5a1756fbfaa61f4ed8d33ce
Atefe <atefe.mosayebi@gmail.com>
2021-01-12 15:15:07 -0800

    moving average from data stream

--- Queue/MovingAverageFromDataStream.java
+++ Queue/MovingAverageFromDataStream.java
@@ -32,7 +32,30 @@ class MovingAverage {
 }
 
 /**
- * Your MovingAverage object will be instantiated and called as such:
- * MovingAverage obj = new MovingAverage(size);
- * double param_1 = obj.next(val);
+second way: keeping the size of queue less or equal the size
+keeping the updated total sum
  */
+
+class MovingAverage2 {
+
+    int size, windowSum = 0, count = 0;
+    Deque<Integer> queue;
+
+    public MovingAverage(int size) {
+        this.size = size;
+        queue = new ArrayDeque<Integer>();
+    }
+
+    public double next(int val) {
+    ++count;
+
+    // calculate the new sum by shifting the window
+    queue.add(val);
+    int tail = (count > size) ? (int)queue.poll() : 0;
+
+    windowSum = windowSum - tail + val;
+
+    return windowSum * 1.0 / Math.min(size, count);\
+        
+    }
+}
