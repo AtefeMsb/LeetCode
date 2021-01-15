64831fcb53a530d63b1f968a7c516cf12b6947ee
Atefe <atefe.mosayebi@gmail.com>
2021-01-14 21:23:42 -0800

    valid paranthesis check

--- Stack/valid-parentheses.java
+++ Stack/valid-parentheses.java
@@ -1,22 +1,29 @@
 class Solution {
     public boolean isValid(String s) {
-      
-        if (s.length() % 2 == 1) return false;
-        Stack<Character> stack = new Stack();
+        
+        if (s.length() % 2 == 1) {
+            return false;
+        }
+        
+        Deque<Character> stack = new ArrayDeque<>();
         
         for (char c : s.toCharArray()) {
             
             if (c == '(' || c == '{' || c == '[') {
                 stack.push(c);
-            } else if (c == ')' && !stack.isEmpty() && stack.peek() == '(') {
-                stack.pop();
-            } else if (c == '}' && !stack.isEmpty() && stack.peek() == '{') {
-                stack.pop();
-            } else if (c == ']' && !stack.isEmpty() && stack.peek() == '[') {
-                stack.pop();
+            } else if (c == ')' && !stack.isEmpty() && stack.peek() == '(')  {
+               stack.pop();
+            } else if (c == '}' && !stack.isEmpty() && stack.peek() == '{')  {
+               stack.pop();
+            } else if (c == ']' && !stack.isEmpty() && stack.peek() == '[')  {
+               stack.pop();
+            } else {
+                return false;
             }
+           
         }
         
         return stack.isEmpty();
+          
     }
 }
