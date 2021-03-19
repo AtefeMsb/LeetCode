a5b8b76fd7a7fb41c1f69f2e620fcdbfd4173f45
Atefe <atefe.mosayebi@gmail.com>
2021-03-18 19:32:23 -0700

    top frequency heap/greedy question - task scheduler

---
+++ Heap/TaskScheduler.java
@@ -0,0 +1,45 @@
+package Heap;
+
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        
+        // count the frequency of each task
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : tasks) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        
+        // put all the frequencie in maxHeap, beuase we want to start from the highest
+        // and spread it through the cycles, to have a more effiecient use of cpu time
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
+        maxHeap.addAll(map.values());
+                
+        int cycles = 0;
+        while (!maxHeap.isEmpty()) {
+            
+            List<Integer> temp = new ArrayList<>();
+            
+            // for the length of cooldown (n) we need new characters to run interchangeably
+            for (int i = 0; i < n + 1; i++) {
+                if (!maxHeap.isEmpty()) {
+                    // this is simulating running the task, by adding it to the list
+                    temp.add(maxHeap.remove());
+                }
+            }
+            
+            // if any of the task still not finished, add back to the heap
+            for (int i : temp) {
+                if (--i > 0) {
+                    maxHeap.add(i);
+                }
+            }
+            
+            // if heap is empty, only need to increment by the num of tasks we ran
+            // but in any other case it takes cooldown cycle amount to run
+            cycles += maxHeap.isEmpty() ? temp.size() : n + 1;
+            
+        }
+        
+        return cycles;   
+    }
+}
