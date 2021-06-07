4a91f258cde3f06e11f7742318367a800cc1e44f
Atefe <atefe.mosayebi@gmail.com>
2021-06-07 12:21:25 -0700

    top frequency array problems

--- Array/maximum-subarray-dp.java
+++ Array/maximum-subarray-dp.java
@@ -1,9 +1,10 @@
 /*
 Dynamic Programming Solution
-for each elemnt checks to see if sum of numbers till then + nums[i] is bigger or if
-start a new subarray from then
+for each element checks to see if sum of numbers till then + nums[i] is bigger or if
+start a new subarray from this element
 Time Complexity: O(n)
 Space Complexity: O(1) - only 2 extra variables are used
+https://github.com/bephrem1/backtobackswe/blob/master/Dynamic%20Programming%2C%20Recursion%2C%20%26%20Backtracking/MaxContiguousSubarraySum/LinearTime.java
 */
 class Solution {
     public int maxSubArray(int[] nums) {
@@ -12,7 +13,6 @@ class Solution {
         int maxEndingHere =  nums[0];
         // biggest of all maxEndingHere's
         int maxSofar = nums[0];
-        
         
         for (int i = 1; i < nums.length ; i++) {
             
@@ -26,7 +26,7 @@ class Solution {
 }
 
 //------------------------------------------------------
-// in this approach the space complexity is O(n) but it is more clear to understand
+// in this approach the space complexity is O(n) but it is more clear to understand compare to the one above
 // dynamic programing with tabulation
 class Solution2 {
     public int maxSubArray(int[] nums) {
--- LinkedList/intersection-of-two-linked-lists.java
+++ LinkedList/intersection-of-two-linked-lists.java
@@ -1,3 +1,10 @@
+/**
+ * TWO POINTERS
+ * time complexity O(N + M)
+ * N: length of list A
+ * M: length of list B
+ * space complexity: O(1)
+ */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         
@@ -10,7 +17,9 @@ public class Solution {
         
         // both pA and pB travel the same amount of nodes
         // after each travel its own list, will start from the head of the other
-        // eventually they meet if there is an intersectio node
+        // eventually they meet if there is an intersection node
+
+        // if intersection was not found (both pointers null) or found (both pointers on intersection), both pointer will be equal and the loop will end
         while (pA != pB) {
             
             if (pA == null) {
@@ -27,5 +36,37 @@ public class Solution {
         }
         
         return pA;   
+    }
+}
+
+/**
+ * HASH SET
+ * time complexity O(N + M)
+ * N: length of list A
+ * M: length of list B
+ * space complexity: O(M) for hash set
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        
+        if (headA == null || headB == null) return null;
+        
+        // put all the nodes from B in a hashset
+        Set<ListNode> set = new HashSet<>();
+        while (headB != null) {
+            set.add(headB);
+            headB = headB.next;
+        }
+        
+        // 2. check all nodes in list A to see if they already exists in hashset for B
+        while (headA != null) {
+            if (set.contains(headA)) {
+                return headA;
+            } 
+            headA = headA.next;
+        }
+        
+        // 3. intersection was not found
+        return null;
     }
 }
