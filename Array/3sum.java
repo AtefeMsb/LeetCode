222095623d9f48e58130fc20b8017ad21c3d7012
Atefe <atefe.mosayebi@gmail.com>
2021-07-22 12:08:16 -0700

    3sum using sorting and two-pointers

--- Array/3sum.java
+++ Array/3sum.java
@@ -1,41 +1,44 @@
-// a + b + c = 0
+/**
+ * using sorted list and two-pointers
+ * time: O(n ^ 2)
+ */
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         
-        List<List<Integer>> results = new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums.length < 3) return result;
         
+        // sort - O(nlogn)
         Arrays.sort(nums);
         
         for (int i = 0; i < nums.length - 2; i++) {
+            int left = i + 1;
+            int right = nums.length - 1;
             
-            // avoiding duplicates in a by ignoring same numbers
-            if(i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
-                int low = i + 1;
-                int high = nums.length - 1;
-                // b + c = 0 - a
-                int sum = 0 - nums[i];
+            // avoid the duplicate i
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            
+            while (left < right) {
                 
-                // using two-pointer technique
-                while (low < high) {
-                    // found a matching triplet
-                    if (nums[low] + nums[high] == sum) {
-                        results.add(Arrays.asList(nums[i], nums[low], nums[high]));
-                        // avoiding duplicates in b by ignoring same numbers
-                        while (low < high && nums[low] == nums[low + 1]) low++; 
-                        // avoiding duplicates in c by ignoring same numbers
-                        while (low < high && nums[high] == nums[high - 1]) high--; 
-                        low++;
-                        high--;
-                        // in case sum is not reached, adjust the boundries
-                    } else if (nums[low] + nums[high] > sum) {
-                        high--;
-                    } else {
-                        low++;
-                    }
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+                    
+                    // avoid the duplicate left and right
+                    while (left < right && nums[left] == nums[left - 1]) left++;
+                    while (left < right && nums[right] == nums[right + 1]) right--;
+                    
+                } else if (sum < 0) {
+                    left++;
+                } else if (sum > 0) {
+                    right--;
                 }
-            } 
+            }
+            
         }
-        
-        return results;
+
+        return result;
     }
 }
