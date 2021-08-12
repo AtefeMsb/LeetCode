829f27537cf9b88a866ab7884decbdf127d6e4bb
Atefe <atefe.mosayebi@gmail.com>
2021-08-12 16:28:22 -0700

    insertdelete o(1) question

--- DynamicProgramming/PerfectSquares.java
+++ DynamicProgramming/PerfectSquares.java
@@ -1,18 +1,32 @@
-package DynamicProgramming;
+/**
+* Time complexity: O(n⋅ square-root(n))
+* In main step, we have a nested loop, where the outer loop is of nn iterations and in the 
+* inner loop it takes at maximum square-root(n) iterations.
+* Space Complexity: O(n)
+*/
 
 class Solution {
     public int numSquares(int n) {
-        int[] dp = new int[n+1];
-        Arrays.fill(dp, Integer.MAX_VALUE);
+        
+        int dp[] = new int[n + 1];
+        for (int i = 1; i < n + 1; i++) {
+            dp[i] = Integer.MAX_VALUE;
+        }
         dp[0] = 0;
         
-        for(int i = 1; i <= n; i++){
-            // square numbers
-            for(int j = 1; j * j <= i; j++){
-                dp[i] = Math.min(dp[i], dp[i - j*j] + 1);
+        // fill the dp array
+        for (int target = 1; target < n + 1; target++) {
+            // try all the possible squares
+            for (int s = 1; s < n + 1; s++) {
+                int square = s * s;
+                // if square is more than target
+                if (target - square < 0) {
+                    break;
+                }
+                dp[target] = Math.min(dp[target], 1 + dp[target - square]);
             }
         }
-        System.out.println(Arrays.toString(dp));
+        
         return dp[n];
     }
 }
---
+++ HashTable/Design/InsertDeleteGetRandom.java
@@ -0,0 +1,51 @@
+class RandomizedSet {
+    // key: val    value: index
+    Map<Integer, Integer> map;
+    List<Integer> list;
+    Random random;
+
+    /** Initialize your data structure here. */
+    public RandomizedSet() {
+        map = new HashMap<>();
+        list = new ArrayList<>();
+        random = new Random();
+        
+    }
+    
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    public boolean insert(int val) {
+        if (map.containsKey(val)) return false;
+        map.put(val, list.size());
+        list.add(val);
+        return true;
+    }
+    
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    public boolean remove(int val) {
+        if (!map.containsKey(val)) return false;
+        // move the lastElement to the place of indexToRemove
+        int indexToRemove = map.get(val);
+        int lastElement = list.get(list.size() - 1);
+        list.set(indexToRemove, lastElement);
+        map.put(lastElement, indexToRemove);
+        // remove the last element from the list and map
+        list.remove(list.size() - 1);
+        map.remove(val);
+        return true; 
+    }
+    
+    /** Get a random element from the set. */
+    public int getRandom() {
+        int index = random.nextInt(list.size());
+        return list.get(index);
+        
+    }
+}
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
