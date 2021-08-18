85fcf3214a10697f5bd782ac6ffb1c5464b21209
Atefe <atefe.mosayebi@gmail.com>
2021-08-18 13:41:32 -0700

    edited meeting room 2

--- Heap/MettingRooms2.java
+++ Heap/MettingRooms2.java
@@ -1,38 +1,38 @@
-package Heap;
+/**
+ * Intuition: we can keep all the rooms in a min heap where the key for the min heap would be the ending time of meeting.
+ * time: O(nlog(n))
+ * space: O(n)
+ */
 
-class MettingRooms2 {
-    public int minMeetingRooms(int[][] intervals) {
-        
-    // Check for the base case. If there are no intervals, return 0
-    if (intervals == null || intervals.length == 0) {
-      return 0;
-    }
-
-    // Min heap - only put the finish time in the heap
-    PriorityQueue<Integer> allocator = new PriorityQueue<>();
-            
-
-    // Sort the intervals by start time
-    Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-        
-
-    // Add the first meeting
-    allocator.add(intervals[0][1]);
-
-    // Iterate over remaining intervals
-    for (int i = 1; i < intervals.length; i++) {
-
-      // If the room due to free up the earliest is free, assign that room to this meeting.
-      if (intervals[i][0] >= allocator.peek()) {
-        allocator.poll();
+class Solution {
+  public int minMeetingRooms(int[][] intervals) {
+      
+      // 1. Sort the given meetings by their start time
+      Arrays.sort(intervals, (a, b) -> (a[0] - b[0]));
+      
+      // 2. Initialize a new min-heap and add the first meeting's ending time to the heap.
+      // We simply need to keep track of the ending times as that tells us when a meeting room will get free.
+      PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+      minHeap.add(intervals[0][1]);
+      
+      // 3. For every meeting room check if the minimum element of the heap i.e. the room
+      // at the top of the heap is free or not.
+      // 4. If the room is free, then we extract the topmost element and
+      // add it back with the ending time of the current meeting we are processing.
+      // 5. If not, then we allocate a new room and add it to the heap.
+      for (int i = 1; i < intervals.length; i++) {
+          int currentMeetingStart = intervals[i][0];
+          int earliestEndingTime = minHeap.peek();
+          if (currentStart >= earliestEnding) {
+              minHeap.poll();
+          } 
+          // if old room is alocated or new room assigned, anyway the earliest ending time must be updated.
+          minHeap.add(intervals[i][1]);
       }
-
-      // If a new room is to be assigned, then also we add to the heap,
-      // If an old room is allocated, then also we have to add to the heap with updated end time.
-      allocator.add(intervals[i][1]);
-    }
-
-    // The size of the heap tells us the minimum rooms required for all the meetings.
-    return allocator.size();
+      
+      // 6. After processing all the meetings, the size of the heap will tell us
+      // the number of rooms allocated. This will be the minimum number of rooms needed 
+      // to accommodate all the meetings.
+      return minHeap.size();
   }
 }
