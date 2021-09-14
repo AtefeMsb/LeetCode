39d9360700285b485242ecbecc802112eb2fd50e
Atefe <atefe.mosayebi@gmail.com>
2021-09-13 17:34:41 -0700

    backtrackin for finding all the paths from src to dest

--- Graph/AllPathsFromSourceToTarget.java
+++ Graph/AllPathsFromSourceToTarget.java
@@ -4,15 +4,15 @@
 class Solution {
     
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-       List<List<Integer>> result = new ArrayList<>();
-       dfs(graph, 0, new ArrayList<Integer>(), result);
-       return result;
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        // all the paths start with 0
+        path.add(0);
+        dfs(graph, 0, path, result);
+        return result;
     }
     
     public void dfs(int[][] graph, int node, List<Integer> path, List<List<Integer>> result) {
-        
-        // 1. choose
-        path.add(node);
         
         // base case - if destination is found
         if (node == graph.length - 1) {
@@ -21,6 +21,8 @@ class Solution {
         }
         
         for (int neighbor : graph[node]) {
+            // 1. choose
+            path.add(neighbor);
             // 2. explore
             dfs(graph, neighbor, path, result);
             // 3. backtrack
