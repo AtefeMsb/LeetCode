add899e1b89d68a36220f255b1f8638a19cb435e
Atefe <atefe.mosayebi@gmail.com>
2021-10-27 14:24:55 -0700

    partition equal subset sum

--- DynamicProgramming/PartitionEqualSubsetSum.java
+++ DynamicProgramming/PartitionEqualSubsetSum.java
@@ -62,3 +62,40 @@ class Solution {
         return result;
     }
 }
+
+// =================================================
+/**
+ * Tabulation
+ */
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int totalSum = 0;
+        // find sum of all array elements
+        for (int num : nums) {
+            totalSum += num;
+        }
+        // if totalSum is odd, it cannot be partitioned into equal sum subset
+        if (totalSum % 2 != 0) return false;
+        
+        int sum = totalSum / 2;
+        int n = nums.length;
+        
+        boolean dp[][] = new boolean[n + 1][sum + 1];
+        
+        // index 0 has no number, so it is not possible to create sum
+        for (int j = 0; j <= sum; j++) dp[n][j] = false;
+        // sum of zero always possible to make
+        for (int i = 0; i <= n; i++) dp[i][0] = true;
+        
+        for (int i = 1; i <= n; i++) {  // index - rows
+            int curr = nums[i - 1];
+            for (int j = 0; j <= sum; j++) {    // sum - cols
+                if (j < curr)   // not pick this index, skip
+                    dp[i][j] = dp[i - 1][j];
+                else    // pick it or not
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - curr];
+            }
+        }
+        return dp[n][sum];
+    }
+}
