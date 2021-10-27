3cb629a4e5fe9ea454d4d2e23a0b337eddb98f57
Atefe <atefe.mosayebi@gmail.com>
2021-10-27 10:52:33 -0700

    house rubber 2

--- DynamicProgramming/HouseRubber2.java
+++ DynamicProgramming/HouseRubber2.java
@@ -3,8 +3,10 @@ class Solution {
         // edge case
         if (nums.length == 1) return nums[0];
         
-        int[] part1 = Arrays.copyOfRange(nums, 1, nums.length);
-        int[] part2 = Arrays.copyOfRange(nums, 0, nums.length - 1);
+       // remove the first element
+       int[] part1 = Arrays.copyOfRange(nums, 1, nums.length);
+       // remove the last element
+       int[] part2 = Arrays.copyOfRange(nums, 0, nums.length - 1);
         
         int res1 = helper(part1);
         int res2 = helper(part2);
@@ -13,6 +15,7 @@ class Solution {
     }
     
     public int helper(int[] nums) {
+        // edge cases
         if (nums.length == 0) return 0;
         if (nums.length == 1) return nums[0];
         
@@ -26,5 +29,37 @@ class Solution {
         
         return dp[nums.length - 1];
         
+    }
+}
+// ---------------------------------------------
+class Solution {
+    public int rob(int[] nums) {
+        // edge case
+        if (nums.length == 1) return nums[0];
+        
+        // remove the first element
+        int[] part1 = Arrays.copyOfRange(nums, 1, nums.length);
+        // remove the last element
+        int[] part2 = Arrays.copyOfRange(nums, 0, nums.length - 1);
+        
+        int res1 = memoization(part1, part1.length - 1, new HashMap<Integer, Integer>());
+        int res2 = memoization(part2, part2.length - 1, new HashMap<Integer, Integer>());
+        
+        return Math.max(res1, res2);  
+    }
+    
+    public int memoization(int[] nums, int start, Map<Integer, Integer> memo) {
+        
+        if (start < 0) {
+            return 0;
+        }
+        
+        if (memo.containsKey(start)) {
+            return memo.get(start);
+        }
+        
+        int temp = Math.max(memoization(nums, start - 1, memo), memoization(nums, start - 2, memo) + nums[start]);
+        memo.put(start, temp);
+        return temp;
     }
 }
