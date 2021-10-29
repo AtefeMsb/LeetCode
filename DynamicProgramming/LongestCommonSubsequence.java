04f09b440d6f6abc9a88fa76a04091a44052f1c1
Atefe <atefe.mosayebi@gmail.com>
2021-10-28 17:39:21 -0700

    lcs

--- DynamicProgramming/LongestCommonSubsequence.java
+++ DynamicProgramming/LongestCommonSubsequence.java
@@ -75,3 +75,44 @@ class Solution {
         
     }
 }
+
+// ----------------------------------------------------------------
+/**
+ * more readable dp by me
+ */
+
+ // fill the table from top to down
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        
+        int[][] dp = new int[text1.length() + 1][text2.length() + 1];
+        
+        // first col is zero
+        for (int i = 0; i < text1.length() + 1; i++) {
+            dp[i][0] = 0;
+        }
+        // first row is zero
+        for (int j = 0; j < text2.length() + 1; j++) {
+            dp[0][j] = 0;
+        }
+        
+        // i and j starts from 1 because thge first row and col in table are zero
+        // BUT in string we need to start from index 0, so we have to compare (i-1) and (j-1) indices
+        for (int i = 1; i < text1.length() + 1; i++) {
+            for (int j = 1; j < text2.length() + 1; j++) {
+             
+             // if found similar characters, add one to the diagnoal element
+             if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+            // if no similar, maximum of element in last row and element in last col
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+              
+            }
+        }
+     
+        return dp[text1.length()][text2.length()];
+        
+    }
+}
