ce4dedd470713109e6ed7d22efb791070fb153a8
Atefe <atefe.mosayebi@gmail.com>
2021-11-26 19:20:38 -0800

    find component in undircted graph

--- UnionFind/NumberofConnectedComponentsInAnUndirectedGraph.java
+++ UnionFind/NumberofConnectedComponentsInAnUndirectedGraph.java
@@ -4,63 +4,37 @@
 class Solution {
     public int countComponents(int n, int[][] edges) {
         
-        UnionFind uf = new UnionFind(n);
+        int component = 0;
         
+        // create and adjacency list
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            graph.put(i, new ArrayList<>());
+        }
         for (int[] edge : edges) {
-            uf.union(edge[0], edge[1]);
+            graph.get(edge[0]).add(edge[1]);
+            graph.get(edge[1]).add(edge[0]);
         }
         
-        return uf.getCount();
-    
+        // start dfs from every unvisited node in the graph
+        Set<Integer> visited = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            if (!visited.contains(i)) {
+                component++;
+                dfs(graph, visited, i);
+            }
+        }
+        
+        return component;  
     }
     
-    class UnionFind {
-        
-        int[] root;
-        int[] rank;
-        int count;
-        
-        public UnionFind(int size) {
-            root = new int[size];
-            rank = new int[size];
-            count = size;
-            
-            for (int i = 0; i < size; i++) {
-                root[i] = i;
-                rank[i] = 1;
+    public void dfs(Map<Integer, List<Integer>> graph, Set<Integer> visited, int node) {
+        visited.add(node);
+        for (int neigh : graph.get(node)) {
+            if (!visited.contains(neigh)) {
+                dfs(graph, visited, neigh);
             }
         }
-        
-        public int find(int x) {
-            if (x == root[x]) {
-                return x;
-            }
-            
-            return root[x] = find(root[x]);
-        }
-        
-        public boolean union(int x, int y) {
-            int rootX = find(x);
-            int rootY = find(y);
-            if (rootX == rootY) return false;
-            if (rootX != rootY) {
-                if (rank[rootX] > rank[rootY]) {
-                    root[rootY] = rootX;
-                    rank[rootX] += rank[rootY];
-                } else {
-                    root[rootX] = rootY;
-                    rank[rootY] += rank[rootX];
-                } 
-                count--;
-            }
-            
-            return true;
-        }
-        
-        public int getCount() {
-            return count;
-        }
-        
     }
 }
 // -----------------------------------------------------------
