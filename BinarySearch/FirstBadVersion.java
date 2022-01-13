6022c5d4906476d04aa9700b245ef947ded5cb68
Atefe <atefe.mosayebi@gmail.com>
2022-01-13 14:17:18 -0800

    first bad version

--- BinarySearch/FirstBadVersion.java
+++ BinarySearch/FirstBadVersion.java
@@ -24,3 +24,24 @@ public class Solution extends VersionControl {
         
     }
 }
+
+// -----------------------------
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+        int result = n;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (isBadVersion(mid)) {
+                result = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        
+        return result;
+    }
+}
