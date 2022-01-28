95478b2d66fefe5b7aa8c1b9f78705756b4d6a54
Atefe <atefe.mosayebi@gmail.com>
2022-01-28 12:04:18 -0800

    roatte array

--- Array/RotateArray.java
+++ Array/RotateArray.java
@@ -1,3 +1,22 @@
+// using extra space
+class Solution {
+    public void rotate(int[] nums, int k) {
+        
+        int n = nums.length;
+        int[] temp = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            int pos = (i + k) % n;
+            temp[pos] = nums[i];
+        }
+        
+        for (int i = 0; i < n; i++) {
+            nums[i] = temp[i];
+        }
+    }
+}
+// -------------------------------------------
+// no extra space - using reverse
 class Solution {
     public void rotate(int[] nums, int k) {
         
