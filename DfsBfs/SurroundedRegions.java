0dae7579da18470dd471a1741f3fb9a460696e11
Atefe <atefe.mosayebi@gmail.com>
2022-02-22 14:41:02 -0800

    surrounded regions

--- DfsBfs/SurroundedRegions.java
+++ DfsBfs/SurroundedRegions.java
@@ -1,3 +1,4 @@
+// DFS
 class Solution {
     public void solve(char[][] board) {
         
@@ -50,3 +51,70 @@ class Solution {
         dfs(r, c - 1, board);
     }
 }
+
+// ----------------------------------------------------------------
+// BFS
+
+public class Solution {
+    public void solve(char[][] board) {
+        
+      if (board == null || board.length == 0) {
+        return;
+      }
+        
+      int rows = board.length;
+      int cols = board[0].length;
+  
+      List<int[]> borders = new LinkedList<>();
+        
+      // Step 1). construct the list of border cells
+      for (int r = 0; r < rows; r++) {
+        borders.add(new int[]{r, 0});
+        borders.add(new int[]{r, cols - 1});
+      }
+      for (int c = 0; c < cols; ++c) {
+        borders.add(new int[]{0, c});
+        borders.add(new int[]{rows - 1, c});
+      }
+  
+      // Step 2). mark the escaped cells
+      for (int[] b : borders) {
+        bfs(board, b[0], b[1]);
+      }
+  
+      // Step 3). flip the cells to their correct final states
+      for (int r = 0; r < rows; ++r) {
+        for (int c = 0; c < cols; ++c) {
+          if (board[r][c] == 'O')
+            board[r][c] = 'X';
+          if (board[r][c] == 'T')
+            board[r][c] = 'O';
+        }
+      }
+    }
+  
+    protected void bfs(char[][] board, int r, int c) {
+       int rows = board.length;
+      int cols = board[0].length;
+      LinkedList<int[]> queue = new LinkedList<>();
+      queue.add(new int[]{r, c});
+  
+      while (!queue.isEmpty()) {
+        int[] pair = queue.remove();
+        int row = pair[0];
+        int col = pair[1];
+        if (board[row][col] != 'O')
+          continue;
+  
+        board[row][col] = 'T';
+        if (col < cols - 1)
+          queue.offer(new int[]{row, col + 1});
+        if (row < rows - 1)
+          queue.offer(new int[]{row + 1, col});
+        if (col > 0)
+          queue.offer(new int[]{row, col - 1});
+        if (row > 0)
+          queue.offer(new int[]{row - 1, col});
+      }
+    }
+  }  
