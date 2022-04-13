e801aee2bf4767bf3d40ecd80067377f31a9f5b9
Atefe <atefe.mosayebi@gmail.com>
2022-04-13 15:00:09 -0700

    minimum subarray sum

--- TwoPointers-SlidingWindow/MinimumSizeSubarraySum.java
+++ TwoPointers-SlidingWindow/MinimumSizeSubarraySum.java
@@ -7,12 +7,14 @@ class Solution {
         
         for (int right = 0; right < nums.length; right++) {
             sum += nums[right];
+
             while (sum >= target) {
                 shortest = Math.min(shortest, right - left + 1);
                 // close the window from the left
                 sum -= nums[left];
                 left++;
             } 
+            
         }
         
         return shortest == Integer.MAX_VALUE ? 0 : shortest;
